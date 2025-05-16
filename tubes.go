0b35bb70c00dd77a6da5d7a1af181ab396840e9b
Nabilahr01 <nabilahrobbaniyah@gmail.com>
2025-05-16 09:11:19 +0700

    Update tubes.go
    2 sort

--- tubes.go
+++ tubes.go
@@ -20,3 +20,30 @@ type Transaksi struct {
 	IDProduks   []int
 	Total       float64
 }
+
+// Urut harga besar ke kecil (selection sort)
+func sortProduk(produk []Produk) {
+	n := len(produk)
+	for i := 0; i < n-1; i++ {
+		idxMin := i
+		for j := i + 1; j < n; j++ {
+			if produk[j].Harga < produk[idxMin].Harga {
+				idxMin = j
+			}
+		}
+		produk[i], produk[idxMin] = produk[idxMin], produk[i]
+	}
+}
+
+// Urut total belanja besar ke kecil (insertion sort)
+func sortTransaksi(transaksi []Transaksi) {
+	for i := 1; i < len(transaksi); i++ {
+		kunci := transaksi[i]
+		j := i - 1
+		for j >= 0 && transaksi[j].Total < kunci.Total {
+			transaksi[j+1] = transaksi[j]
+			j--
+		}
+		transaksi[j+1] = kunci
+	}
+}
